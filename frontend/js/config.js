43c3ecf6206cbc963bf2978211e6d6ab17c48581
Laurentiu <laurentiu.luca@migratorydata.com>
2021-11-05 14:30:38 +0200

    update server address.

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,7 +3,7 @@ var USER_ID = getUserId();
 var TOKEN = "some-token";
 
 var SERVERS = [
-    "http://127.0.0.1:8800",
+    "https://ke.migratorydata.com",
 ];
 
 var KAFKA_QUESTION_TOPIC = "question";
